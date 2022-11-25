f7d1fa279fbd631e491f99fb058c69448936cf6d
Kaye Xie <xiekaye@gmail.com>
2022-11-24 20:40:56 -0500

    edit footer

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import PokemonList from "../components/PokemonList";
 import TypeMenu from "../components/TypeMenu";
 import Cart from "../components/Cart";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <TypeMenu />
       <PokemonList />
       <Cart />
+      <Footer />
     </div>
   );
 };
